7dcbf84df7a0d503d2fd5e7ff1dfcb72a70c2346
thuk12gdth-hash <thuk12gdth@gmail.com>
2025-12-10 11:08:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Nông Thị Thơm - Trường Tiểu học B Linh Hồ- ĐT: 0062433934</p>
 </div>
 """, unsafe_allow_html=True)
 
